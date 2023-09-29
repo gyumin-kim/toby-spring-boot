fe9f0bc19e6489d08152f092265d13a94c45bd03
Gyumin Kim <starkying@gmail.com>
2023-09-30 00:15:02 +0900

    front controller 활용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -16,17 +17,27 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext ->
-			servletContext.addServlet("hello", new HttpServlet() {
+		WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController();
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 							  @Override
 							  protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-								  String name = req.getParameter("name");
-								  resp.setStatus(HttpStatus.OK.value());
-								  resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-								  resp.getWriter().println("Hello " + name);
+								  // 인증, 보안, 다국어 공통 기능 등
+								  if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+									  String name = req.getParameter("name");
+									  String ret = helloController.hello(name);
+									  resp.setStatus(HttpStatus.OK.value());
+									  resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+									  resp.getWriter().println(ret);
+								  } else if (req.getRequestURI().equals("/user")) {
+									  // ..
+								  } else {
+									  resp.setStatus(HttpStatus.NOT_FOUND.value());
+								  }
 							  }
 						  })
-						  .addMapping("/hello"));	// url mapping
+						  .addMapping("/*");	// front controller url mapping
+		});
 		webServer.start();
 	}
 
