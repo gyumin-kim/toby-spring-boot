a8ed2533d50a658c80acc3498d3faa956fac7907
Gyumin Kim <starkying@gmail.com>
2023-09-29 18:14:45 +0900

    'hello' servlet 등록 및 url 매핑

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,11 +3,27 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
 		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = tomcatServletWebServerFactory.getWebServer();
+		WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+							  @Override
+							  protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+								  resp.setStatus(200);
+								  resp.setHeader("Content-Type", "text/plain");
+								  resp.getWriter().println("Hello Servlet");
+							  }
+						  })
+						  .addMapping("/hello");	// url mapping
+		});
 		webServer.start();
 	}
 
