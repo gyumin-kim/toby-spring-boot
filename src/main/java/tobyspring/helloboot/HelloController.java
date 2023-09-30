e02555a83d2805514df6bf808b30988c326e03f1
Gyumin Kim <starkying@gmail.com>
2023-09-30 23:44:38 +0900

    HelloController mapping 설정

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,14 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+// DispatcherServlet이 url 매핑 정보를 만들 때, 1. class level, 2. method level의 순서로 참고하므로,
+// @GetMapping과는 별도로 클래스에도 선언 필요
+@RequestMapping
 public class HelloController {
     private final HelloService helloService;
 
@@ -9,6 +16,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(final String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -8,11 +8,13 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		// Spring container
 		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);	// bean 설정 정보 등록
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();	// ApplicationContext(Spring container) 초기화, bean object 생성
 
+		// Servlet container
 		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext ->
 			servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
