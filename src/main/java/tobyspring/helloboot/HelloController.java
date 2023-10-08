844b74b6188c326fea9c6b4521f8c82518b4a72f
Gyumin Kim <starkying@gmail.com>
2023-10-08 17:19:32 +0900

    component로 설정, @ComponentScan 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,6 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,6 +10,7 @@ import java.util.Objects;
 // DispatcherServlet이 url 매핑 정보를 만들 때, 1. class level, 2. method level의 순서로 참고하므로,
 // @GetMapping과는 별도로 클래스에도 선언 필요
 @RequestMapping
+@Component
 public class HelloController {
     private final HelloService helloService;
 
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,24 +2,16 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@ComponentScan
 // @Configuration을 갖는 클래스는 AnnotationConfig 기반의 ApplicationContext에 처음으로 등록됨
 // @Configuration: @Bean 설정 정보가 포함된 클래스임을 Spring container에 알리기 위함
 @Configuration
 public class HellobootApplication {
-	@Bean
-	public HelloController helloController(HelloService helloService) {
-		return new HelloController(helloService);
-	}
-
-	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService();
-	}
 
 	public static void main(String[] args) {
 		// Spring container
