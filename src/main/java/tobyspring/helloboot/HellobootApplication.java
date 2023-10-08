79e53f3a4779638112b79fa80e1aaaca31205ad1
Gyumin Kim <starkying@gmail.com>
2023-10-08 17:08:19 +0900

    Java 코드(configuration 클래스)로 bean 수동 등록

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -2,14 +2,28 @@ package tobyspring.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+// @Configuration을 갖는 클래스는 AnnotationConfig 기반의 ApplicationContext에 처음으로 등록됨
+// @Configuration: @Bean 설정 정보가 포함된 클래스임을 Spring container에 알리기 위함
+@Configuration
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 		// Spring container
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -22,8 +36,7 @@ public class HellobootApplication {
 				webServer.start();
 			}
 		};
-		applicationContext.registerBean(HelloController.class);	// bean 설정 정보 등록
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(HellobootApplication.class);	// 구성 정보를 갖는 Java 클래스 등록
 		applicationContext.refresh();	// ApplicationContext(Spring container) 초기화, bean object 생성
 	}
 
