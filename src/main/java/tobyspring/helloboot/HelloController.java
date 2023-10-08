c5a2014ded5d3a3f1a68040c56e79e6cdc31b4a0
Gyumin Kim <starkying@gmail.com>
2023-10-08 17:21:10 +0900

    @Component를 meta annotation으로 갖는 stereotype annotation으로 변경

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,16 +1,11 @@
 package tobyspring.helloboot;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-// DispatcherServlet이 url 매핑 정보를 만들 때, 1. class level, 2. method level의 순서로 참고하므로,
-// @GetMapping과는 별도로 클래스에도 선언 필요
-@RequestMapping
-@Component
+@RestController
 public class HelloController {
     private final HelloService helloService;
 
@@ -19,7 +14,6 @@ public class HelloController {
     }
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello(final String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
