87394c83cd0747ed27de7e42b520ca298cd1c8c1
Gyumin Kim <starkying@gmail.com>
2023-10-08 16:42:15 +0900

    Spring container 초기화 과정(`onRefresh()`)에서 Servlet container 생성, Servlet 초기화 작업을 같이 수행하도록 수정
    * AbstractApplicationContext의 refresh()에서 Spring container 초기화 작업을 진행
    * template method pattern 형태인 refresh() 내에서 onRefresh()를 호출하며, container 초기화 과정에서 부가적으로 할 작업이 있을 경우 onRefresh()를 override하여 처리
    * onRefresh() comment: "Initialize other special beans in specific context subclasses"

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -9,18 +9,22 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		// Spring container
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+				// Servlet container
+				TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = tomcatServletWebServerFactory.getWebServer(
+					servletContext -> servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+													.addMapping("/*")    // front controller url mapping
+				);
+				webServer.start();
+			}
+		};
 		applicationContext.registerBean(HelloController.class);	// bean 설정 정보 등록
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh();	// ApplicationContext(Spring container) 초기화, bean object 생성
-
-		// Servlet container
-		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext ->
-			servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
-						  .addMapping("/*")	// front controller url mapping
-		);
-		webServer.start();
 	}
 
 }
